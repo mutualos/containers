5da69ccf7e8e87a37d78ef8686e57acd91732037
mutualos <138887864+mutualos@users.noreply.github.com>
2023-10-22 18:43:33 -0400

    Update loan.js
    fix added JSON.parse

--- loan.js
+++ loan.js
@@ -87,7 +87,7 @@ function _1cost_of_funds(columns, header_) {
         $payment = _1estimate_payment(columns, header_);
     }
     let $months = Math.max(Math.min(_1remaining_life_in_months(columns, header_), 360), 1);
-    let COFR_map_ = document.getElementById('treasury_curve_').innerHTML.trim();
+    let COFR_map_ = JSON.parse(document.getElementById('treasury_curve_').innerHTML);
     let principal_sum = 0;
     let paydown = 0;
     let month = 1;
